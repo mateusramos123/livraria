0eae6857cd815138f13c1066f1a33dff74d7d95a
mateusramos123 <mateus.oliveira.ifc@gmail.com>
2025-05-17 10:44:25 -0300

    Formatação dos arquivos do projeto.

--- config/settings.py
+++ config/settings.py
@@ -12,14 +12,14 @@ https://docs.djangoproject.com/en/5.2/ref/settings/
 
 from pathlib import Path
 
-# Build paths inside the project like this: BASE_DIR / 'subdir'.
+
 BASE_DIR = Path(__file__).resolve().parent.parent
 
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/5.2/howto/deployment/checklist/
 
-# SECURITY WARNING: keep the secret key used in production secret!
+
 SECRET_KEY = 'django-insecure-3cy4s9_2khmcp@r4s0fdhon&tc$(!bh8+nl)#gr)s-oz993u%p'
 
 # SECURITY WARNING: don't run with debug turned on in production!
